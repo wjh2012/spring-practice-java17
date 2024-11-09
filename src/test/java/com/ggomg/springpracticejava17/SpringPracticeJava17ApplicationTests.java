2d1a3ce24d5d8600ecb02850b6640fc16e457749
wjh2012 <wjh2012@naver.com>
2024-11-09 18:05:01 +0900

    Initial commit

---
+++ src/test/java/com/ggomg/springpracticejava17/SpringPracticeJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggomg.springpracticejava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeJava17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
